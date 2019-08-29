b76ceff2ff3228d1ae93ed9fac463268dbae17e2
8hal <mkm8421@gmail.com>
2019-08-29 10:29:06 +0900

    component food like and foodlist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-function Food({ fav }) {
-console.log(fav);
-  return <h1>I like {fav}</h1>
+function FoodLike(Food) {
+console.log(Food);
+  return <div>
+    <h1>I like {Food.name}</h1>
+    <img src={Food.image} alt = {Food.name}/>
+  </div>
+  
 }
+
+const FoodList = [
+    {
+      key : 1,
+      name : "kimchi",
+      image : "http://lorempixel.com/400/200/food/1/"
+    },
+    {
+      key : 2,
+      name : "udon",
+      image : "http://lorempixel.com/400/200/food/2/"
+    },
+    {
+      key : 3,
+      name : "sushi",
+      image : "http://lorempixel.com/400/200/food/3/"
+
+    },
+  ]
 
 function App() {
   return <div>
-    <h1>Hello!!</h1>
-    <Food fav='kimchi'/>
-    <Food fav='donut'/>
-    <Food fav='soju'/>
+    {FoodList.map(FoodLike)}
     </div>;
 }
 
