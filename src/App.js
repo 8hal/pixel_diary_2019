ddfa900f9c451a34bc907f8122e06ea62b33830a
8hal <mkm8421@gmail.com>
2019-09-02 17:45:55 +0900

    formated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React from 'react';
 
 
-class App extends React.Component{
-constructor(props){
-  super(props);
-  console.log("hello");
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
 
   state = {
-  count : 0,
-  bgColor : "orange"
-}
+    count: 0,
+    bgColor: "orange"
+  }
 
-add = () => {
-  this.setState(current => ({bgColor : "pink"}));
- };
-minus = () => {
-  this.setState(current => ({bgColor : "gray"}));
-};
-componentDidMount(){
-  console.log("component rendered");
-}
-componentDidUpdate(){
-  console.log("component updated");
-}
-  render(){
+  add = () => {
+    this.setState(current => ({ bgColor: "pink" }));
+  };
+
+  minus = () => {
+    this.setState(current => ({ bgColor: "gray" }));
+  };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("component updated");
+  }
+
+  render() {
     console.log("rendering");
-    
-return <div>
-<div style={{backgroundColor:this.state.bgColor}}>today</div>
-<button style={{backgroundColor: "pink"}} onClick = {this.add} >Happy</button>
-<button style={{backgroundColor: "gray"}} onClick = {this.minus} >Sad</button>
-</div>
+
+    return <div>
+      <div style={{ backgroundColor: this.state.bgColor }}>today</div>
+      <button style={{ backgroundColor: "pink" }} onClick={this.add} >Happy</button>
+      <button style={{ backgroundColor: "gray" }} onClick={this.minus} >Sad</button>
+    </div>
 
   }
 }
